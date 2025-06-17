34cc155e1b98494cd2891c97686e64f7f86f3c52
James Anderson <joji@ando.ws>
2025-06-18 00:58:15 +0100

    Removed unneeded Fabric API dependency.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 <h3 align="center">BeaconSaturation</h3>
 
 ## Features
-Regeneration from beacons will now also applies Saturation. Only required server-side.
+Regeneration from beacons now also applies Saturation. Only required server-side.
 
 Optionally install on client-side for hunger bar animation that matches the health bar's regeneration wave.
--- src/client/java/yungando/beaconsaturation/mixin/client/InGameHudMixin.java
+++ src/client/java/yungando/beaconsaturation/mixin/client/InGameHudMixin.java
@@ -18,8 +18,7 @@ public class InGameHudMixin {
   private int ticks;
 
   @Inject(method = "renderFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getHungerManager()Lnet/minecraft/entity/player/HungerManager;", ordinal = 1))
-  public void renderFood(DrawContext context, PlayerEntity player, int top, int right, CallbackInfo ci,
-      @Local(ordinal = 3) int j, @Local(ordinal = 4) LocalIntRef k) {
+  public void renderFood(DrawContext context, PlayerEntity player, int top, int right, CallbackInfo ci, @Local(ordinal = 3) int j, @Local(ordinal = 4) LocalIntRef k) {
     if (player.hasStatusEffect(StatusEffects.SATURATION)) {
       if (j == (this.ticks % MathHelper.ceil(25f))) {
         k.set(k.get() - 2);
